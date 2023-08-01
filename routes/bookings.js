293ac5a67ba2391a7bdd21dbbe2c34576517e353
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-01 16:25:16 +0700

    init

---
+++ routes/bookings.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  createBooking,
+  getAllBooking,
+  getBooking,
+} from "../Controllers/bookingController.js";
+import { verifyAdmin, verifyUser } from "../utils/authenticate.js";
+
+const router = express.Router();
+
+router.post("/", verifyUser, createBooking);
+router.get("/:id", verifyUser, getBooking);
+router.get("/", verifyAdmin, getAllBooking);
+
+export default router;
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getSingleUser,
+  updateUser,
+} from "../Controllers/userController.js";
+
+import { verifyAdmin, verifyUser } from "../utils/authenticate.js";
+
+const router = express.Router();
+
+//Update user
+router.put("/:id", verifyUser, updateUser);
+
+//Delete user
+router.delete("/:id", verifyUser, deleteUser);
+
+//Get single user
+router.get("/:id", verifyUser, getSingleUser);
+
+//Get all user
+router.get("/", verifyAdmin, getAllUser);
+
+export default router;
