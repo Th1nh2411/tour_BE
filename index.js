26b4f4f8b7c95b13089a2ffc7be3d170cc6df460
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-23 09:02:10 +0700

    vercel

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const corsOptions = {
 mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb+srv://rinktvn2525:0905138221thinh@cluster0.dpawfmv.mongodb.net/tours_booking?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
