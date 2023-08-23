33b59021cde29ba17cde55d788c7e6bd345c8d26
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-23 11:18:37 +0700

    fix vercel

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const corsOptions = {
 mongoose.set("strictQuery", false);
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
