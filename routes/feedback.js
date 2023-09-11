e49ca4820d7860621625223e52430db251bfd9a4
ManhPM <phammanhbeo2001@gmail.com>
2023-09-11 16:28:08 +0700

    add feedback, modifine guide

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { getAllFeedback, createFeedback } from '../controllers/feedbackController.js';
-import { verifyToken } from '../utils/authenticate.js';
+import { verifyAdmin, verifyToken } from '../utils/authenticate.js';
 
 const router = express.Router();
-router.get('/', getAllFeedback);
+router.get('/', verifyToken, verifyAdmin, getAllFeedback);
 router.post('/', verifyToken, createFeedback);
 
 export default router;
