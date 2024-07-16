e280dc400807072baa06ea99443489f843655b82
Th1nh2411 <rinktvn2525@gmail.com>
2024-07-16 14:35:05 +0700

    disable auth active

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ const router = express.Router();
 
 //Update user
 router.put('/profile', checkExistPhoneNumber, verifyToken, updateUser);
-router.post('/active', active);
+// router.post('/active', active);
 router.put('/changepassword', verifyToken, changePassword);
 router.post('/forgotpassword', forgotPassword);
 router.post('/forgotpassword/verify', verify);
